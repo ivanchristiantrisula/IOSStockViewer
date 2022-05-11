0c8648b8e9cd1c58d18b0de48d71fa5e5158f7e9
Ivan Christian <ivanchristiantrisula@outlook.com>
2022-05-11 19:21:28 +0700

    Initial Commit

---
+++ StockPrice/StockPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceApp.swift
+//  StockPrice
+//
+//  Created by Ivan Christian on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
